8e25cfd915a6e3c51a3d175478f19b3850680335
Michael Dunlap <michaeldunlap@earthlink.net>
2017-06-15 11:53:05 -0700

    create html table gird with background image

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-function Space (x,y) {
+//business logic
+
+function Space (x,y,id) {
 	this.x = x;
   this.y = y;
+	this.id = id;
 }
 
 function Board () {
@@ -26,15 +29,15 @@ Player.prototype.getMark = function() {
   return this.mark;
 }
 
-var space1 = new Space (0,0);
-var space2 = new Space (0,1);
-var space3 = new Space (0,2);
-var space4 = new Space (1,0);
-var space5 = new Space (1,1);
-var space6 = new Space (1,2);
-var space7 = new Space (2,0);
-var space8 = new Space (2,1);
-var space9 = new Space (2,2);
+var space1 = new Space (0,0,space1);
+var space2 = new Space (0,1,space2);
+var space3 = new Space (0,2,space3);
+var space4 = new Space (1,0,space4);
+var space5 = new Space (1,1,space5);
+var space6 = new Space (1,2,space6);
+var space7 = new Space (2,0,space7);
+var space8 = new Space (2,1,space8);
+var space9 = new Space (2,2,space9);
 
 
 var myBoard = new Board();
@@ -51,3 +54,9 @@ myBoard.board.push(space9);
 
 
 console.log(myBoard.findSpace(1,2));
+
+//interface logic
+
+$(document).ready(function () {
+	alert ("hi");
+});
